4e6483e6f728fad746a72848ea4f23a310d838c2
anshukumar11062 <anshu8581@gmail.com>
2024-05-21 13:46:42 +0530

    Admin Dashboard Implemented

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
+    Route::get('/admin-dashboard', function () {
+        return view('admin.dashboard');
     })->name('dashboard');
 });
